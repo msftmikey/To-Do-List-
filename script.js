603544a4d64c96716f888f1f7696855160446410
Miguel M <113144211+msftmikey@users.noreply.github.com>
2023-08-24 14:42:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskForm = document.getElementById("task-form");
+    const taskList = document.getElementById("task-list");
+
+    taskForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const taskInput = document.getElementById("task");
+        const dueDateInput = document.getElementById("due-date");
+
+        const taskName = taskInput.value;
+        const dueDate = dueDateInput.value;
+
+        if (taskName.trim() === "") {
+            alert("Task name cannot be empty!");
+            return;
+        }
+
+        const taskItem = document.createElement("li");
+        taskItem.innerHTML = `
+            <span>${taskName}</span>
+            <span>Due: ${dueDate}</span>
+            <button class="delete-button">Delete</button>
+        `;
+
+        taskList.appendChild(taskItem);
+        taskInput.value = "";
+        dueDateInput.value = "";
+    });
+
+    taskList.addEventListener("click", function (event) {
+        if (event.target.classList.contains("delete-button")) {
+            const taskItem = event.target.closest("li");
+            taskItem.remove();
+        }
+    });
+});

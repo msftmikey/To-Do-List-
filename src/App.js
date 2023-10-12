a5351467d81c381d4e317032cd91980d82e372a2
alextran55 <alexandertran55@csu.fullerton.edu>
2023-10-12 13:12:05 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -17,6 +17,11 @@ const firebaseConfig = {
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 export const database = firebaseApp.database();
 
+function formatDate(dateString) {
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  return new Date(dateString).toLocaleDateString('en-US', options);
+}
+
 function App() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
@@ -46,81 +51,87 @@ function App() {
     alert(`Task: ${taskName}\nDue Date: ${taskDueDate}`);
   };
 
+  useEffect(() => {
+    // Reference to the 'tasks' node in Firebase Realtime Database
+    const tasksRef = firebase.database().ref('tasks');
 
-useEffect(() => {
-  // Reference to the 'tasks' node in Firebase Realtime Database
-  const tasksRef = firebase.database().ref('tasks');
+    // Set up a listener to retrieve tasks from Firebase
+    tasksRef.on('value', (snapshot) => {
+      const tasksData = snapshot.val();
 
-  // Set up a listener to retrieve tasks from Firebase
-  tasksRef.on('value', (snapshot) => {
-    const tasksData = snapshot.val();
+      if (tasksData) {
+        // Convert the tasks object to an array
+        const tasksArray = Object.entries(tasksData).map(([taskId, taskData]) => ({
+          id: taskId,
+          ...taskData,
+        }));
 
-    if (tasksData) {
-      // Convert the tasks object to an array
-      const tasksArray = Object.entries(tasksData).map(([taskId, taskData]) => ({
-        id: taskId,
-        ...taskData,
-      }));
-
-      setTasks(tasksArray);
-    }
-  });
-
-  // Clean up the listener when the component unmounts
-  return () => {
-    tasksRef.off('value');
-  };
-}, []);
-
-
-const handleTaskDelete = (taskId) => {
-  // Reference to the task to be deleted in Firebase Realtime Database
-  const taskRef = firebase.database().ref(`tasks/${taskId}`);
-  // Remove the task from Firebase
-  taskRef.remove()
-    .then(() => {
-      // Update local state after successfully deleting the task from the database
-      setTasks(tasks.filter(task => task.id !== taskId));
-    })
-    .catch(error => {
-      console.error("Error removing task: ", error);
+        setTasks(tasksArray);
+      }
     });
-};
 
+    // Clean up the listener when the component unmounts
+    return () => {
+      tasksRef.off('value');
+    };
+  }, []);
+
+  const handleTaskDelete = (taskId) => {
+    // Reference to the task to be deleted in Firebase Realtime Database
+    const taskRef = firebase.database().ref(`tasks/${taskId}`);
+    // Remove the task from Firebase
+    taskRef.remove()
+      .then(() => {
+        // Update local state after successfully deleting the task from the database
+        setTasks(tasks.filter(task => task.id !== taskId));
+      })
+      .catch(error => {
+        console.error("Error removing task: ", error);
+      });
+  };
 
   return (
     <div className="container">
-      <h1>To-Do List</h1>
+      <div className='sidebar'>
       <form onSubmit={handleAddTask}>
-        <label htmlFor="task">Task:</label>
-        <input
-          type="text"
-          id="task"
-          name="task"
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-          required
-        />
-        <label htmlFor="due-date">Due Date:</label>
-        <input
-          type="date"
-          id="due-date"
-          name="due-date"
-          value={dueDate}
-          onChange={(e) => setDueDate(e.target.value)}
-        />
-        <button type="submit">Add Task</button>
-        {error && <p className="error">{error}</p>}
-      </form>
-      <ul id="task-list">
-  {tasks.map((task) => (
-    <li key={task.id}>
-      {task.name} -- {task.dueDate}
-      <button onClick={() => handleTaskOpen(task.name, task.dueDate)}>Open Task</button>
-      <button onClick={() => handleTaskDelete(task.id)}>Delete Task</button>
-    </li>
-  ))}
-</ul>
+          <label htmlFor="task">Task:</label>
+          <input
+            type="text"
+            id="task"
+            name="task"
+            autoComplete='off'
+            placeholder=' Enter task name'
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            required
+          />
+          <label htmlFor="due-date"></label>
+          <input
+            type="date"
+            id="due-date"
+            name="due-date"
+            value={dueDate}
+            onChange={(e) => setDueDate(e.target.value)}
+          />
+          <button type="submit">Add Task</button>
+          {error && <p className="error">{error}</p>}
+        </form>
+      </div>
+      
+      <div className='content'>
+        <ul id="task-list">
+            {tasks.map((task) => (
+              <li key={task.id}>
+                <p><b>{task.name}</b><br></br> Due {formatDate(task.dueDate)}</p>
+                <div class='list_buttons'>
+                  <button id='open_in_new' class="material-symbols-outlined" onClick={() => handleTaskOpen(task.name, task.dueDate)}>open_in_new</button>
+                  <button id='delete' class="material-symbols-outlined" onClick={() => handleTaskDelete(task.id)}>delete</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+      </div>
+
     </div>
   );
 }
